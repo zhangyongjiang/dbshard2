71b7c813a1f6b8f7a6c33974911f1dc59a632a14
Kevin Zhang <zhangyongjiang@gmail.com>
2016-01-15 09:24:04 -0800

    less locking chance

--- src/main/java/com/gaoshin/dbshard2/ExtendedDataSource.java
+++ src/main/java/com/gaoshin/dbshard2/ExtendedDataSource.java
@@ -26,99 +26,105 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.log4j.Logger;
 
 public class ExtendedDataSource extends BasicDataSource {
-	private static Logger logger = Logger.getLogger(ExtendedDataSource.class);
-	
-	private ThreadLocal<RequestContext> threadContext = new ThreadLocal<RequestContext>();
-	private Map<RequestContext, Connection> connections = new HashMap<RequestContext, Connection>();
+    private static Logger logger = Logger.getLogger(ExtendedDataSource.class);
+    
+    private ThreadLocal<RequestContext> threadContext = new ThreadLocal<RequestContext>();
+    private Map<RequestContext, Connection> connections = new HashMap<RequestContext, Connection>();
 
-	private int dataSourceId;
-	private boolean autoCommit;
+    private int dataSourceId;
+    private boolean autoCommit;
 
-	public void setThreadContext(RequestContext tc) {
-		threadContext.set(tc);
-	}
-	
-	public int getDataSourceId() {
-		return dataSourceId;
-	}
+    public void setThreadContext(RequestContext tc) {
+        threadContext.set(tc);
+    }
+    
+    public int getDataSourceId() {
+        return dataSourceId;
+    }
 
-	public void setDataSourceId(int dataSourceId) {
-		this.dataSourceId = dataSourceId;
-	}
-	
-	@Override
-	public Connection getConnection() throws SQLException {
-		synchronized (connections) {
-			RequestContext context = getThreadContext().get();
-			if(context != null) {
-                Connection conn = connections.get(context);
-    			if(conn == null) {
-    				conn = super.getConnection();
-    				conn.setAutoCommit(isAutoCommit());
-    				connections.put(getThreadContext().get(), conn);
-    				getThreadContext().get().addDataSource(this);
-    				logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + getThreadContext().get() + " connection " + conn.hashCode() + " create.");
-    			}
-    			else {
-    				logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + getThreadContext().get() + " connection " + conn.hashCode() + " reuse.");
-    			}
-                return conn;
-			}
-			else {
-			    Connection conn = super.getConnection();
+    public void setDataSourceId(int dataSourceId) {
+        this.dataSourceId = dataSourceId;
+    }
+    
+    @Override
+    public Connection getConnection() throws SQLException {
+        RequestContext context = getThreadContext().get();
+            if(context != null) {
+                synchronized (context) {
+                    Connection conn = null;
+                    synchronized (connections) {
+                        conn = connections.get(context);
+                    }
+                    if(conn == null) {
+                        conn = super.getConnection();
+                        conn.setAutoCommit(isAutoCommit());
+                        synchronized (connections) {
+                            connections.put(getThreadContext().get(), conn);
+                        }
+                        getThreadContext().get().addDataSource(this);
+                        logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + getThreadContext().get() + " connection " + conn.hashCode() + " create.");
+                    }
+                    else {
+                        logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + getThreadContext().get() + " connection " + conn.hashCode() + " reuse.");
+                    }
+                    return conn;
+                }
+            }
+            else {
+                logger.debug(">>>>>>>>>>> No RequestContext found. set auto commit to true");
+                Connection conn = super.getConnection();
                 conn.setAutoCommit(true);
                 return conn;
-			}
-		}
-	}
-	
-	public void closeConnection(RequestContext rc) {
-		Connection connection = null;
-		synchronized (connections) {
-			connection = connections.get(rc);
-			connections.remove(rc);
-		}
-		logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " close.");
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void commitConnection(RequestContext rc) {
-		Connection connection = null;
-		synchronized (connections) {
-			connection = connections.get(rc);
-		}
-		logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " commit.");
-		try {
-			connection.commit();
-		} catch (SQLException e) {
-			logger.warn("cannot commit", e);
-		}
-	}
-	
-	public void rollbackConnection(RequestContext rc) {
-		Connection connection = null;
-		synchronized (connections) {
-			connection = connections.get(rc);
-		}
-		logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " commit.");
-		try {
-			connection.rollback();;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+            }
+    }
+    
+    public void closeConnection(RequestContext rc) {
+        Connection connection = null;
+        synchronized (connections) {
+            connection = connections.get(rc);
+            connections.remove(rc);
+        }
+        logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " close.");
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void commitConnection(RequestContext rc) {
+        Connection connection = null;
+        synchronized (connections) {
+            connection = connections.get(rc);
+        }
+        logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " commit.");
+        try {
+            connection.commit();
+        } catch (SQLException e) {
+            logger.warn("cannot commit", e);
+        }
+    }
+    
+    public void rollbackConnection(RequestContext rc) {
+        Connection connection = null;
+        synchronized (connections) {
+            connection = connections.get(rc);
+        }
+        logger.debug(">>>>>>>>>>>dataSourceId " + dataSourceId + " request " + rc + " connection " + connection.hashCode() + " commit.");
+        try {
+            connection.rollback();;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public boolean isAutoCommit() {
-		return autoCommit;
-	}
+    public boolean isAutoCommit() {
+        return autoCommit;
+    }
 
-	public void setAutoCommit(boolean autoCommit) {
-		this.autoCommit = autoCommit;
-	}
+    public void setAutoCommit(boolean autoCommit) {
+        this.autoCommit = autoCommit;
+    }
 
     public ThreadLocal<RequestContext> getThreadContext() {
         return threadContext;
